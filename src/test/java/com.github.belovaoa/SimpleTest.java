5f22c3897acebc48e0b9e6bbd6510cc59e539273
belova_oa <belovva.o@gmail.com>
2021-11-24 14:43:57 +0300

    added first simple test

---
+++ src/test/java/com.github.belovaoa/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.github.belovaoa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
