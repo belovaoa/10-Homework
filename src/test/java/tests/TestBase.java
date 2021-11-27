ef6deb0b9e4524f977086016759ed9c10df1ce41
belova_oa <belovva.o@gmail.com>
2021-11-27 22:31:53 +0300

    убрали лишнее

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,8 +3,6 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
-import java.util.Locale;
-
 public class TestBase {
 
     RegistrationForm registrationForm = new RegistrationForm();
