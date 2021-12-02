0b1f55841231764647b21d6974fe08f76ec6d58f
belova_oa <belovva.o@gmail.com>
2021-12-02 08:06:21 +0300

    added changes

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -2,8 +2,10 @@ package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.componentsOfCalenderAndAttach.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,9 +17,12 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
 
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
         // Configuration.remote = format("");
+        // Configuration.remote = format("https://%s:%s@%s&quot;, credentials.login(), credentials.password(),
+        //System.getProperty("url")
          Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
